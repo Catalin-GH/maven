729fd5857cf7729bfdd9b57f40ab1fc41a527c43
Catalin-GH <32494506+Catalin-GH@users.noreply.github.com>
2024-07-11 13:52:29 +0300

    add StringUtils dependency

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Maven Startup");
+        System.out.println(StringUtils.upperCase("hello world!"));
     }
 }
